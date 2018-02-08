8fcb1e519ccdbf9ccd62f76ac966d0b89f661ff8
Lamont Granquist <lamont@scriptkiddie.org>
2018-02-07 20:28:35 -0800

    fix another perf bug
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- lib/chef/node/attribute.rb
+++ lib/chef/node/attribute.rb
@@ -1,7 +1,7 @@
 #--
 # Author:: Adam Jacob (<adam@chef.io>)
 # Author:: AJ Christensen (<aj@chef.io>)
-# Copyright:: Copyright 2008-2017, Chef Software Inc.
+# Copyright:: Copyright 2008-2018, Chef Software Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -235,10 +235,12 @@ class Chef
       end
 
       def reset
+        #puts "TOTAL RESET"
         @deep_merge_cache = nil
       end
 
       def reset_cache(*path)
+        #puts "PATH: #{path}"
         if path.empty?
           reset
         else
